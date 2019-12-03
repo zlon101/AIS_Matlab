46a227ca0adf8fa851f9ade60f05261b9935f882
czl <2513023434@qq.com>
2019-12-03 23:32:03 +0800

    add .bat 自动执行

--- CostDistor/calcuDist.m
+++ CostDistor/calcuDist.m
@@ -7,8 +7,9 @@ function [distortion,residual] =  calcuDist(coverImg,stegoImg)
 % [rhoP1,rhoM1] = CostUNIWD(coverPath);
 [rhoP1,rhoM1] = CostHILL(coverImg);
 
-residual = single(stegoImg) - single(coverImg);
-rhoM1 = rhoM1(residual==-1);
-rhoP1 = rhoP1(residual==1);
+% residual = single(stegoImg) - single(coverImg);
+rhoM1 = rhoM1(single(stegoImg)-single(coverImg)==-1);
+rhoP1 = rhoP1(single(stegoImg)-single(coverImg)==1);
 distortion = sum(rhoM1) + sum(rhoP1);
+residual = 0;
 end
