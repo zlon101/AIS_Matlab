86607581eb63ae26112584043fe290d48ce509de
czl <2513023434@qq.com>
2019-12-15 10:02:31 +0800

    updata

--- embedAlg/HUGO_matlab/CostHUGO.m
+++ embedAlg/HUGO_matlab/CostHUGO.m
@@ -24,11 +24,6 @@ rhoP1 = zeros(size(coverImg),'single');  % declare cost of +1 change
 for row=1:size(coverImg, 1)
   for col=1:size(coverImg, 2)
     D_P1 = 0; D_M1 = 0;
-%     [C_Rez_H(row+3, col:col+5);
-%      C_Rez_V(row:row+5, col+3);
-%       C_Rez_H(row+3, col:col+5);
-%       C_Rez_H(row+3, col:col+5)];
-
     % Horizontal
     c_rez_sub = C_Rez_H(row+3, col:col+5)';
     stego_sub_P1 = c_rez_sub + responseP1;
@@ -53,34 +48,34 @@ for row=1:size(coverImg, 1)
     stego_sub_M1 = c_rez_sub - responseP1;
     D_M1 = D_M1 + GetLocalDistortion(c_rez_sub, stego_sub_M1, params);
     % D_P1 = D_P1 + GetLocalDistortion(cover_sub, stego_sub_P1, params);
+    
     rhoM1(row, col) = D_M1;
     rhoP1(row, col) = D_P1;            
   end
 end
 % clear S_Rez_H S_Rez_V S_Rez_Diag S_Rez_MDiag C_Rez_H C_Rez_V C_Rez_Diag;
-
 rhoP1 = rhoM1;
 rhoM1(rhoM1>wetCost) = wetCost;
 rhoP1(rhoP1>wetCost) = wetCost;
 rhoP1(coverImg == 255) = wetCost;
 rhoM1(coverImg == 0) = wetCost;
 
+%% Ç¶Ì×º¯Êý
   function D = GetLocalDistortion(C_resVect, S_resVect, params)
     % C_resVect: cover image µÄ²Ð²î¾ØÕó
-    % C_resVect and S_resVect must have size of 6x1   
-    params.sigma = var(C_resVect(:));
+    %params.sigma = var(C_resVect(:));
     D = 0;
-    D = D + GetLocalPotential(C_resVect(1:3), S_resVect(1:3), params);
-    D = D + GetLocalPotential(C_resVect(2:4), S_resVect(2:4), params);
-    D = D + GetLocalPotential(C_resVect(3:5), S_resVect(3:5), params);
-    D = D + GetLocalPotential(C_resVect(4:6), S_resVect(4:6), params);
+    D= D+ GetLocalPotential(C_resVect, S_resVect, params);
+    %for i=1:4
+    %  D= D+ GetLocalPotential(C_resVect(i:i+2), S_resVect(i:i+2), params);
+    %end
   end
 
   function Vc = GetLocalPotential(c_res, s_res, params)
     % sqrt ¸ÄÎª abs
+    params.sigma = single(1);
     params.gamma=single(1);
-    % params.sigma = 1;
-    c_w = (params.sigma + sqrt(sum(c_res.^2))) .^ (-params.gamma);
+    c_w = (params.sigma + norm(c_res)) .^ (-params.gamma);
     s_w=single(0);
     % s_w = (params.sigma + sqrt(sum(s_res.^2))) .^ (-params.gamma);
     Vc = (c_w + s_w);
--- embedAlg/HUGO_matlab/mextest.m
+++ embedAlg/HUGO_matlab/mextest.m
@@ -2,15 +2,14 @@
 % matlab coder mex function
 cover = single(imread('E:\astego\Images\Experis\195.pgm'));
 payload=single(0.2);
-message_length = round(numel(cover)*payload);
 
-HUGO_OPT_EMBEDS('E:\astego\Images\BOSS_ALL\', 'E:\astego\Images\HUGOOPT_04\',single(0.4));
 
+[rhoP1,rhoM1] = CostHUGO(cover);
+P = 1./rhoP1;
+P2 = filter2(ones(7), P,'same');
+nChange= round(numel(cover)*payload);
+clear payload 
+% HUGO_OPT_EMBEDS('E:\astego\Images\BOSS_ALL\', 'E:\astego\Images\HUGOOPT_04\',single(0.4));
 % [stego, distortion] = HUGO(cover, payload);
-% imshow(stego-cover,[]);
-% [rhoP1,rhoM1] = CostHUGO(cover);
-% figure('name','HUGO_+1¸ÅÂÊ'); imshow(rhoP1.\1,[]);
-
 % stego = EmbeddingSimulator(cover, rhoP1, rhoM1, message_length, false);
-% [stego, distortion] = HUGO(cover, single(0.4), single([1.1,1.2]));
 
